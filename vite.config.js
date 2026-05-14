7289b338fdd77fbbf5efe37a97a2d8c637f5529f
Jerry Chen <example@email.com>
2026-05-14 11:33:09 +0800

    fix: 修复资源路径问题，改用相对路径

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
-  base: '/MyBlog/',
+  base: './',
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': '/src'
+      '@': path.resolve(__dirname, './src')
     }
   },
   server: {
